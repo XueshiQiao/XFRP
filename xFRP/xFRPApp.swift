6b3afe3a72e1c41f8c13a0cb56210fea13c7c031
joey <qiaoxueshi@gmail.com>
2024-09-07 00:25:39 +0800

    Initial Commit

---
+++ xFRP/xFRPApp.swift
@@ -0,0 +1,17 @@
+//
+//  xFRPApp.swift
+//  xFRP
+//
+//  Created by Xueshi Qiao on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct xFRPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
